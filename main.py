9ebcbd9f2605bd7c6f7f85ed093803ccbf17808b
nourhammmad <nournourt614@gmail.com>
2023-12-27 23:22:58 +0200

    Final 2.0

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import tkinter as tk
 import tkinter.messagebox
 from tkinter import *
 import math
-
+import simpleaudio as sa
 
 class AlertFSM:
     def __init__(self):
@@ -44,12 +44,6 @@ class AlertFSM:
 
 fsm = AlertFSM()
 
-
-
-
-
-
-
 def on_slider_change(value):
     my_upd(value)
     # Update timer value in AlertFSM based on slider value
@@ -67,10 +61,9 @@ def on_slider_change(value):
         fsm.transition("smokepresisted")
         labelGood.config(text="Smoke Presisted")
         print("smokep")
-    elif (fsm.timer >= 120 and fsm.timer < 180):
+    elif (fsm.timer >= 120 and fsm.timer <= 180):
         fsm.transition('Alert')
         fsm.transition('ringBuzzer')
-        #play_sound()
         fsm.transition('sendNotification')
         if (fsm.timer >= 120 and fsm.timer < 155):
             fsm.transition("sprinkleWater")
@@ -78,8 +71,13 @@ def on_slider_change(value):
             fsm.transition("fireoff")
             labelGood.config(text="Handled by water")
             labelGood.place(x=165, y=290)
+            tkinter.messagebox.showinfo("Fire", "FIRE DETECTION")
+            for i in range(10):
+                if i==5:
+                    break
             tkinter.messagebox.showinfo("Fire", "Fire is off")
             slider.set(0)
+            my_upd(0)
         else:
             fsm.transition("sprinkleWater")
             fsm.transition("checkfire")
@@ -88,25 +86,16 @@ def on_slider_change(value):
             labelGood.config(text="Emergency")
             labelGood.place(x=175, y=290)
             tkinter.messagebox.showwarning("Fire", "CALL 911")
+            play_sound()
             slider.set(0)
-
-
-        # labelGood.config(text="Fire")
-        # labelGood.place(x=175, y=290)
-        # tkinter.messagebox.showwarning("Fire","FIRE")
-        # fsm.transition('reset')
-        # slider.set(0)
-        # my_upd(0)
+            my_upd(0)
 # Slider widget
 root = tk.Tk()
 root.geometry("686x391")
 root.resizable(False, False)
 root.title("Fire Alert")  # Set the title of the window
 root.configure(bg='white')
-# bg=PhotoImage(file=b"background.png")
-#
-# label1 = Label(root,image=bg)
-# label1.place(x=0,y=0)
+
 width,height=410,310 # set the variables
 d=str(width)+"x"+str(height+40)
 root.geometry(d)
@@ -116,23 +105,24 @@ x1,y1,x2,y2=35,35,355,355 # dimensions for the arc
 x,y,r=195,195,135
 c1=tk.Canvas(root,width=width-10,height=height-50,bg='#FFFFFF',highlightbackground="white")
 c1.grid(row=0,column=0)
-#x1,y1,x2,y2=25,30,370,370
 
-res=1 # resolution or steps
-#for d in range(0,180,res):
- #   my_c.create_arc(x1, y1,x2,y2, start=d, extent=res+1,outline=my_upd(d),width=arc_w,style=tk.ARC)
+res=1
 def on_slider_click(event):
     # Calculate the slider position based on the click event
     position = event.x / slider_width
     print(event.x)
-    slider.set(position-30)
+    if(position==0):
+        slider.set(position)
+    else:
+        slider.set(position-30)
 
 def my_upd(value):
     global line
     in_radian = math.radians(slider.get()) # scale value in radian
+    if(slider.get()==0):
+        labelGood.config(text="Idle")
     c1.delete(line) # delete the pointer
-    line=c1.create_line(x,y,(x+r*-math.cos(in_radian)),
-            (y-r*math.sin(in_radian)),width=6,arrow='last')
+    line=c1.create_line(x,y,(x+r*-math.cos(in_radian)),(y-r*math.sin(in_radian)),width=6,arrow='last')
 
 def my_updd(d):
     change=int((255/180)*d) # Jump in colour value
@@ -142,26 +132,22 @@ res=1 # resolution or steps
 for d in range(0,180,res):
     c1.create_arc(x1, y1,x2,y2, start=d, extent=res+1,outline=my_updd(d),
         width=arc_w,style=tk.ARC)
-# c1.create_arc(x1, y1,x2,y2, start=0, extent=45,outline='red',width=arc_w,style=tk.ARC)
-# c1.create_arc(x1, y1,x2,y2, start=45, extent=45,outline='orange', width=arc_w,style=tk.ARC)
-# c1.create_arc(x1, y1,x2,y2, start=90, extent=30,outline='yellow', width=arc_w,style=tk.ARC)
-# c1.create_arc(x1, y1,x2,y2, start=120, extent=60,outline='green',width=arc_w,style=tk.ARC)
 
 # small circle at center
 c1.create_oval(x-10,y-10,x+10,y+10,fill='black')
 in_radian=math.radians(180) # getting radian value
-line=c1.create_line(x,y,(x+r*math.cos(in_radian)),
-        (y-r*math.sin(in_radian)),width=6,arrow='last')
+line=c1.create_line(x,y,(x+r*math.cos(in_radian)),(y-r*math.sin(in_radian)),width=6,arrow='last')
 
 # Slider configuration
 # length_label = Label(root, text="Fire")
-slider = tk.Scale(root, from_=0, to=180, orient=tk.HORIZONTAL, command=on_slider_change,length=300,bg='white',showvalue=False,highlightbackground="white")
+slider = tk.Scale(root, from_=0, to=180, orient=tk.HORIZONTAL, command=on_slider_change,length=300,bg='white',showvalue=False,highlightbackground="white",troughcolor="red", activebackground="blue")
 slider.grid(row=2,column=0)
 slider_width = slider.winfo_width()
 print(slider_width)
 slider.bind("<Button-1>", on_slider_click)
 labelGood=tk.Label(root,font=("Bernard MT Condensed", 16),bg='white')
 labelGood.place(x=175, y=290)
+
 def play_sound():
     try:
         wave_obj = sa.WaveObject.from_wave_file("buzzer.mp3")  # Replace with the path to your sound file
